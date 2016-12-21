1ccd117eab39b1c815077da2cf334e157ba2403a
Piotrekk85 <piotr.jastr@gmail.com>
2016-12-21 17:30:25 +0100

    Add files via upload

---
+++ WpfApplication19/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace WpfApplication19
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Osoba osoba1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            osoba1 = new Osoba();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DateTime dataP = DateTime.ParseExact(DataUrodzenia.Text,"dd.MM.yyyy",CultureInfo.InvariantCulture);
+                osoba1.Ustaw(Imie.Text, Nazwisko.Text, dataP);
+                Opis.Text=osoba1.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("coś nie tak");
+            }
+        }
+    }
+}
---
+++ WpfApplication19/Osoba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace WpfApplication19
+{
+    class Osoba
+    {
+        private string imie;
+        private string nazwisko;
+        private DateTime dataUrodzenia;
+
+        public Osoba() { }
+
+        public void Ustaw(string imie, string nazwisko, DateTime data)
+        {
+            this.imie = imie;
+            this.nazwisko = nazwisko;
+            this.dataUrodzenia = data;
+        }
+
+        public int ObliczWiek()
+        {
+            return DateTime.Now.Year - dataUrodzenia.Year;
+        }
+
+        public override string ToString()
+        {
+            return imie + "\n" + nazwisko + Environment.NewLine + dataUrodzenia.ToString("dd.MM.yyyy",CultureInfo.InvariantCulture) + Environment.NewLine + ObliczWiek();
+        }
+    }
+}
